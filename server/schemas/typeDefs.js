d7240eb79a84a71ced21cfdc2e958e43931986f0
JoshuaHamann <joshwashku@yahoo.com>
2023-03-29 17:32:07 -0500

    added apollo client to front end and added some pseudo code to resolver

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID 
+        authors: [String]
+        description: String
+        title: String
+        # image:
+        # link:
+    }
+
+    type Auth {
+        
+    }
+`;
+
+module.exports = typeDefs;

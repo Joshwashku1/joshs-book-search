3ecad69f04e02e96b0354f12b10c5690f4938a10
JoshuaHamann <joshwashku@yahoo.com>
2023-04-02 23:40:55 -0500

    added a graphql mutation to the front end that handles login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,7 +24,7 @@ const resolvers = {
             }
 
             const token = signToken(user);
-            return { token, profile };
+            return { token, user };
         },
         addUser: async(parent, {username, email, password}) => {
             const newUser = User.create( { username, email, password })

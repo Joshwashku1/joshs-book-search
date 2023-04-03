ce454d24d7e89d76b918b83cd64eb5aad8dec21f
JoshuaHamann <joshwashku@yahoo.com>
2023-04-02 23:33:52 -0500

    added login logic to server in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,8 +11,20 @@ const resolvers = {
 
     Mutation: {
         login: async (parent, {email, password}) => {
+            const user = await User.findOne({ email });
+
+            if(!user) {
+                throw new AuthenticationError('No user with this email found!');
+            }
             
-            return;
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            const token = signToken(user);
+            return { token, profile };
         },
         addUser: async(parent, {username, email, password}) => {
             const newUser = User.create( { username, email, password })
